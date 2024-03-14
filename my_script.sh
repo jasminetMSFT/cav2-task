3132176b52ce5118958409cccc708fef53d5269b
Jasmine Tang <110378437+jasminetMSFT@users.noreply.github.com>
2024-03-14 16:21:24 +0800

    Update my_script.sh

--- my_script.sh
+++ my_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Authenticate using managed identity
-az login --identity
+az login --identity --username 7c74ceb5-652b-4bbc-8ed5-69ac97340a9c
 
 # Show account information
 az account show
